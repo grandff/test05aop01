e01016c62a703d5fe590b595ad5e2ca20f066d7a
김정민 <dsaqaz421@naver.com>
2020-06-09 17:11:32 +0900

    aop 기본

--- test05aop01/src/main/java/test/com/aop01/HomeController.java
+++ test05aop01/src/main/java/test/com/aop01/HomeController.java
@@ -7,6 +7,7 @@ import java.util.Locale;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,8 @@ public class HomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	@Autowired
-	private TestDAOimpl dao;
+	@Qualifier("proxyBean")		// proxyBean 연결
+	private TestDAO dao;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
--- test05aop01/src/main/java/test/com/aop01/TestMethodInterceptorImpl.java
+++ test05aop01/src/main/java/test/com/aop01/TestMethodInterceptorImpl.java
@@ -14,14 +14,20 @@ public class TestMethodInterceptorImpl implements MethodInterceptor {
 		logger.info("invoke on !!!! ");
 		
 		// 1. 메소드 실행 전에 메소드명 획득
+		String method = invocation.getMethod().getName();			// method 이름 가져옴 
+		logger.info("1. call method name is " + method);
 		
 		// 2. 메소드 실행 전 수행로직 구현
+		logger.info("2. {} 메소드 실행[전] 수행 로직 구현", method);
 		
 		// 3. 해당 메소드 호출
+		Object obj = invocation.proceed();
+		logger.info("3. called {} return {} ", method, obj);
 		
 		// 4. 메소드 실행 후 수행 로직 구현
+		logger.info("4. {} 메소드 실행 [후] 수행 로직 구현", method);
 		
-		return null;
+		return obj;
 	}
 
 }
